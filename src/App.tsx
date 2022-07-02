198e5799ee5acfdab48e3b0d6ca9ecc903958318
李婷婷 Lee Ting Ting <tleeae@connect.ust.hk>
2022-07-02 22:14:07 +0000

    hiii

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Hello Tina</h3>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
